a012b0dfc34e63aa6643f7d3e1d1636303be539d
Nancy Hunter <144382501+Nancy-Hunter@users.noreply.github.com>
2023-10-19 16:16:32 -0400

    Add files via upload

---
+++ APIsite3/js/main.js
@@ -0,0 +1,66 @@
+
+document.querySelector('button').addEventListener('click', getFetch)
+//adds event listener for input bar (click)
+document.querySelector("html").addEventListener("keypress", function (press) {
+	if (press.key === "Enter") {
+		getFetch();
+	}
+}); //adds event listener for input bar (click)
+document.querySelector('li').addEventListener('click', getFetch)
+//adds event listener for nav bar (click) DOESNT WORK nav bar needs anchor tags? maybe a class? needs to change choice to clicked value instead of input value
+
+function getFetch(){
+  const choice = document.querySelector('input').value.toLowerCase() //user input
+  const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='+ choice //appends api endpoint with user choice
+
+  
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data) 
+
+        document.querySelector('img').src = `${data.drinks[0].strDrinkThumb}` //adds picture to dom
+
+        document.querySelector('h2').innerText = data.drinks[0].strDrink //adds drink name to dom
+
+        document.querySelector('.steps').innerText = data.drinks[0].strInstructions //adds drink instructions to dom
+
+        for (let i = 1; i<= 15; i++) { //adds list of ingredients
+          let ingredients = data.drinks[0][`strIngredient${i}`]
+          let measurements = data.drinks[0][`strMeasure${i}`]
+
+          if (measurements && ingredients) { //adds ingredients with measurements 
+
+            let li = document.createElement('li')
+
+            li.innerHTML = `${measurements} ${ingredients}`
+
+            document.querySelector('ul').append(li)
+            
+          } else if (ingredients) { //adds ingredients without measurements
+            let li = document.createElement('li')
+
+            li.innerHTML = ingredients
+
+            document.querySelector('#ingredients').append(li)
+          }
+        }
+        for (let i=1; i<data.drinks.length; i++) { //adds similar query responses to nav bar
+
+            let li = document.createElement('li')
+
+            li.textContent = data.drinks[i].strDrink
+
+            document.querySelector('#otherBevs').append(li)
+        }
+         
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+
+      document.querySelector('main').classList.remove('off')
+      
+}
+
